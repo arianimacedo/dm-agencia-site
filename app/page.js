2aefd5aff554372e08d2a02cc5c1bf0df9a416a8
ariani.paulino <ariani.paulino@gmail.com>
2025-01-16 15:54:59 -0300

    Remove arquivos SVG e arquivos de estilo globais da aplicação, alteração da pasta pages para raiz do projeto

---
+++ app/page.js
@@ -0,0 +1,24 @@
+"use client";
+
+import Home from "../src/components/Home";
+import Services from "../src/components/Services";
+import About from "../src/components/About";
+import Projects from "../src/components/Projects";
+import Steps from "../src/components/Steps";
+import Testimonials from "../src/components/Testmonials";
+import Cta from "../src/components/Cta";
+
+export default function Page() {
+  return (
+    <>     
+      <Home />
+      <Services />
+      <About /> 
+      <Projects />
+      <Steps />
+      <Testimonials />
+      <Cta />
+     
+    </>
+  );
+}
